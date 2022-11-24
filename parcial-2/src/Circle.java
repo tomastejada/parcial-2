e53ba494fbfd2fcaab0e8739a5e24567a3e3f2a1
tomastejada <tomas.tejada@davinci.edu.ar>
2022-11-24 14:41:29 -0300

    add: class and getters & setters

---
+++ parcial-2/src/Circle.java
@@ -0,0 +1,30 @@
+public class Circle extends Shape {
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    private double radius;
+
+    public Double circle() {
+        return null;
+    }
+
+    public Double getArea () {
+        return null;
+    }
+
+    public Double getPerimeter () {
+        return null;
+    }
+
+
+
+
+
+
+
+}
---
+++ parcial-2/src/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle extends Shape {
+
+    private double width;
+    private double lenght;
+
+    public Rectangle(double width, double lenght) {
+        this.width = width;
+        this.lenght = lenght;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    
+
+}
